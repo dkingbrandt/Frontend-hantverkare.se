2a26e2597201d00aea0c42f9e2878b31c9f9a5bd
BexRyd <becka_91_@hotmail.com>
2022-08-18 18:44:20 +0100

    routes app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
-
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Categories from "./components/Categories";
+import Adds from "./components/Adds";
+import Footer from "./components/Footer";
+import Header from "./components/Header"
+
 
 function App() {
+
+
+    <div>
+        <Header/>
+        <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Categories"element={<Categories/>}/>
+        <Route path="/Adds" element={<Adds />} />
+        
+      </Routes>
+      <Footer/>
+    </div>
   
 }
 
