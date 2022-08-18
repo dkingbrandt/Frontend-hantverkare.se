9bd1204afb77896864859872b09afb98d95ad52d
dkingbrandt <dkingbrandt@hotmail.com>
2022-08-18 19:58:01 +0200

    first output header

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
+    <div className="App">
+      <Header>
+        
+    </Header>
 
 
 
 
     <Footer>
 
-    </Footer>
+      </Footer>
+      </div>
   )
 }
 
