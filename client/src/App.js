6227e7942862455aff32cfb8c7ea42ec07dfdf55
dkingbrandt <dkingbrandt@hotmail.com>
2022-08-19 11:28:59 +0200

    giving header to arthur

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,11 @@ import Home from "./components/Home";
 import Categories from "./components/Categories";
 import Adds from "./components/Adds";
 import Footer from "./components/Footer";
-import Header from './components/Header';
 
 function App() {
   return (
     <div className='appContainer'>
-      <Header/>
+      
      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Categories"element={<Categories />} />
