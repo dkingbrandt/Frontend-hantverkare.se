3c01fbbe8d22133708e3c00a57fdfff93c34399b
Annika Hägglund <annika.a.a.hagglund@outlook.com>
2022-08-25 00:36:40 +0200

    layout categories

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,38 +1,48 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-import "../css/header.css"
+import "../css/header.css";
 
 function Header() {
   return (
-    <div className='appContainer'>
+    <div className="appContainer">
       <Navbar expand="lg">
-      <Container fluid>
-        <Navbar.Brand className="logo" href="/">hantverkare.se</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav>
-            <Nav.Link className='navlink' href="/">Hem</Nav.Link>
-            <Nav.Link className='navlink' href="Categories">Sätt in annons</Nav.Link>
-            <Nav.Link className='navlink' href="Adds">Mina annonser</Nav.Link>
-          </Nav>
-          <Form className="d-flex btn_main">
-            <Form.Control
-              type="search"
-              placeholder="Sök"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button href="/AddInfo" variant="dark">Logga in</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Container fluid>
+          <Navbar.Brand className="logo" href="/">
+            hantverkare.se
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav>
+              <Nav.Link className="navlink" href="/">
+                Hem
+              </Nav.Link>
+              <Nav.Link className="navlink" href="Adds">
+                Sätt in annons
+              </Nav.Link>
+              <Nav.Link className="navlink" href="Adds">
+                Mina annonser
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex btn_main">
+              <Form.Control
+                type="search"
+                placeholder="Sök"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button href="/AddInfo" variant="dark">
+                Logga in
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-  )
+  );
 }
 
 export default Header;
