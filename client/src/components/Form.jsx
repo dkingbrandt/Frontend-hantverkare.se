58b4f8b9fd83e812b5bad1b23fb4b9249d2ffbf2
Eijey11 <arturs.jarovojs2018@gmail.com>
2022-08-27 17:05:13 +0200

    Added inputs to form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -6,12 +6,23 @@ function FormPopUp() {
   return (
 <Form>
       <Form.Group className="mb-2" controlId="exampleForm.ControlInput1">
-        <Form.Label>Ange epost</Form.Label>
+        <Form.Label>Namn</Form.Label>
+        <Form.Control type="text" placeholder="Skriv namn" />
+      </Form.Group>
+      <Form.Group className="mb-2" controlId="exampleForm.ControlInput1">
+        <Form.Label>Telefonnummer</Form.Label>
+        <Form.Control type="phone" placeholder="Skriv telefonnummer" />
+      </Form.Group>
+      <Form.Group className="mb-2" controlId="exampleForm.ControlInput1">
+        <Form.Label>Epost</Form.Label>
         <Form.Control type="email" placeholder="name@example.com" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Meddelande</Form.Label>
         <Form.Control as="textarea" placeholder='Skriv meddelande' rows={3} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Bekräfta" />
       </Form.Group>
 
       <Form.Group className='btn_form_container'>
